2b00b6716035bb6d1aff2544860cd207b8a29f3c
Neil Gentleman <ngentleman@gmail.com>
2012-09-29 08:57:05 -0700

    month tick skeleton

--- public/Game.js
+++ public/Game.js
@@ -1,6 +1,7 @@
 
 function Game() {
 	this.budget = 1000;
+	this.consumed = 0;
 	this.equipment = {};
 	this.occupants = [];
 	for (var i = 0; i < 10; i++) {
@@ -8,3 +9,16 @@ function Game() {
 	}
 	this.month = 0;
 }
+
+Game.prototype.monthDelta = function (weather) {
+	var total = { money: 0, happy: 0, energy: 0 };
+	_.each(this.equipment, function (equip) {
+		var partial = equp.monthDelta(this, weather);
+		total.money += partial.money;
+		total.happy += partial.happy;
+		total.energy += partial.energy;
+	});
+	this.budget += total.money;
+	this.consumed += total.energy;
+	this.applyHappiness(total.happy);
+};
