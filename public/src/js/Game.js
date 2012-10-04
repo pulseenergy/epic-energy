158a35fa6b447135ec4a4bbee09c797b8e78c72f
David Hirtle <david.hirtle@pulseenergy.com>
2012-10-03 21:33:03 -0700

    extract data

--- public/src/js/Game.js
+++ public/src/js/Game.js
@@ -1,14 +1,6 @@
 var debug = false;
-var vancouverWeather = {
-	recordHigh: [15.6, 16.4, 19.1, 23.9, 32.7, 30, 31.7, 31.7, 28.5, 24.2, 17, 15, 32.7],
-	averageHigh: [6.8, 8.4, 10.6, 13.5, 16.8, 19.6, 22.0, 22.3, 19.0, 13.9, 9.3, 6.8, 14.1],
-	mean: [4.8, 5.9, 7.6, 10, 13.2, 15.9, 18.1, 18.3, 15.4, 11.1, 7.1, 4.8, 11],
-	averageLow: [2.7, 3.4, 4.6, 6.5, 9.5, 12.2, 14.1, 14.4, 11.6, 8.2, 4.8, 2.8, 7.9],
-	recordLow: [-13.3, -6.7, -5, -1.1, 1.1, 2.8, 2.8, 5, 1.7, -3.2, -9.9, -15.6, -15.6]
-};
 
 var startingBudget = 10000;
-var vancouverOfficeEnergy = [50985, 43655, 42752, 37045, 35019, 34887, 38875, 39735, 33185, 35012, 48868, 53010]; // kwh
 var occupantNames = ["Rohit", "Bob", "Suzie", "Cher", "Ryan", "Carlos", "Shane", "Dan", "Donalda", "Angie", "Theresa", "John", "Gorp"];
 
 function Game() {
@@ -30,8 +22,8 @@ function Game() {
 
 	// later: these could be dynamic for a given location
 	// disaggregated data down below would require using fixed end use percentages
-	this.weather = vancouverWeather;
-	this.baselineEnergy = vancouverOfficeEnergy;
+	this.weather = data.weather.vancouver;
+	this.baselineEnergy = data.baselineEnergy.vancouver;
 	this.disaggregatedBaseline = this.disaggregate(this.baselineEnergy, this.baselineEnergy);
 	this.messages = [];
 	this.appendMessages(this.messages, ["It's cold in here. I'm not getting enough heat."]);
@@ -248,7 +240,7 @@ Game.prototype.computeScore = function () {
 };
 
 function getBaselineTotal() {
-	return _.reduce(vancouverOfficeEnergy, function (sum, amount) {
+	return _.reduce(data.baselineEnergy.vancouver, function (sum, amount) {
 		return sum + amount;
 	}, 0);
 }
